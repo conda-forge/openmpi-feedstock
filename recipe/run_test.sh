cb8897690a129be46a47e34758325220186988ae
Min RK <benjaminrk@gmail.com>
2019-02-05 16:24:35 +0100

    test components in subpackages

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 set -ex
 
-command -v ompi_info
-ompi_info
-
-command -v mpicc
-mpicc -show
-
-command -v mpicxx
-mpicxx -show
-
-command -v mpifort
-mpifort -show
-
-command -v mpif90
-mpif90 -show
-
-command -v mpif77
-mpif77 -show
-
 export OMPI_MCA_plm=isolated
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 export OMPI_MCA_rmaps_base_oversubscribe=yes
 
 command -v mpiexec
 MPIEXEC="${PWD}/mpiexec.sh"
-$MPIEXEC --help
 
 pushd "tests"
 
 if [[ $PKG_NAME == "openmpi" ]]; then
+  command -v ompi_info
+  ompi_info
+
+  $MPIEXEC --help
+
   $MPIEXEC -n 4 python test_exec.py
 fi
 
 if [[ $PKG_NAME == "openmpi-mpicc" ]]; then
+  command -v mpicc
+  mpicc -show
+
   mpicc $CFLAGS $LDFLAGS helloworld.c -o helloworld_c
   $MPIEXEC -n 4 ./helloworld_c
 fi
 
 if [[ $PKG_NAME == "openmpi-mpicxx" ]]; then
+  command -v mpicxx
+  mpicxx -show
+
   mpicxx $CXXFLAGS $LDFLAGS helloworld.cxx -o helloworld_cxx
   $MPIEXEC -n 4 ./helloworld_cxx
 fi
 
 if [[ $PKG_NAME == "openmpi-mpifort" ]]; then
+
+  command -v mpifort
+  mpifort -show
+
+  command -v mpif90
+  mpif90 -show
+
+  command -v mpif77
+  mpif77 -show
+
   mpif77 $FFLAGS $LDFLAGS helloworld.f -o helloworld_f
   $MPIEXEC -n 4 ./helloworld_f
 
