b44b157291c6f8e03b19a998433805dd6b10ecf7
Min RK <benjaminrk@gmail.com>
2019-12-05 15:59:41 +0100

    disable C++ bindings
    OMPI seems to officially discourage using them
    as they have been removed from the MPI standard many years ago

--- recipe/build-mpi.sh
+++ recipe/build-mpi.sh
@@ -15,7 +15,6 @@ export FC=$(basename "$FC")
 if [ $(uname) == Darwin ]; then
     if [[ ! -z "$CONDA_BUILD_SYSROOT" ]]; then
         export CFLAGS="$CFLAGS -isysroot $CONDA_BUILD_SYSROOT"
-        export CXXFLAGS="$CXXFLAGS -isysroot $CONDA_BUILD_SYSROOT"
     fi
     export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
 
@@ -32,7 +31,6 @@ export LIBRARY_PATH="$PREFIX/lib"
 
 ./configure --prefix=$PREFIX \
             --disable-dependency-tracking \
-            --enable-mpi-cxx \
             --enable-mpi-fortran \
             --disable-wrapper-rpath \
             --disable-wrapper-runpath \
--- recipe/tests/helloworld.cxx
+++ recipe/tests/helloworld.cxx
@@ -3,12 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-  MPI::Init();
+  int provided, size, rank, len;
+  char name[MPI_MAX_PROCESSOR_NAME];
 
-  int size = MPI::COMM_WORLD.Get_size();
-  int rank = MPI::COMM_WORLD.Get_rank();
-  int len; char name[MPI_MAX_PROCESSOR_NAME];
-  MPI::Get_processor_name(name, len);
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Get_processor_name(name, &len);
+
+  printf("Hello, World! I am process %d of %d on %s.\n", rank, size, name);
 
   std::cout <<
     "Hello, World! " <<
@@ -17,6 +21,6 @@ int main(int argc, char *argv[])
     " on  "          << name <<
     "."              << std::endl;
 
-  MPI::Finalize();
+  MPI_Finalize();
   return 0;
 }
