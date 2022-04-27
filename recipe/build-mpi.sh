266efbdff35665dc419f5646ab6ac4c2c215cfb9
Isuru Fernando <isuruf@gmail.com>
2022-04-26 20:52:48 -0500

    Update recipe/build-mpi.sh
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- recipe/build-mpi.sh
+++ recipe/build-mpi.sh
@@ -24,8 +24,8 @@ fi
 if [[ -z $CUDA_HOME ]]; then
     build_with_cuda=""
 else
-    export CFLAGS="$CFLAGS -I$CUDA_HOME"
-    export CXXFLAGS="$CXXFLAGS -I$CUDA_HOME"
+    export CFLAGS="$CFLAGS -I$CUDA_HOME/include"
+    export CXXFLAGS="$CXXFLAGS -I$CUDA_HOME/include"
     build_with_cuda="--with-cuda --with-ucx=$PREFIX"
 fi
 
