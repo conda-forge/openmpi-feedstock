3c69da01db3fd77a3cd8f27912eb0d4d266d3042
Min RK <benjaminrk@gmail.com>
2024-06-05 15:13:16 +0200

    FCFLAGS is FFLAGS

--- recipe/openmpi_activate.sh
+++ recipe/openmpi_activate.sh
@@ -6,6 +6,9 @@ if [[ "${CONDA_BUILD:-}" = "1" ]]; then
       echo "OMPI_${_var}=${!_var}"
       export OMPI_${_var}="${!_var}"
     fi
+    if [[ -z "${OMPI_FCFLAGS}" && ! -z "${FFLAGS}" ]]; then
+      export OMPI_FCFLAGS="$FFLAGS"
+    fi
   done
   export OPAL_PREFIX="$PREFIX"
 
