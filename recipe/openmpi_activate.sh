51203d342100f9f005cd82a2bdf583dc21bd2875
Min RK <benjaminrk@gmail.com>
2024-06-08 17:32:35 +0200

    only set wrapper args for cross compilation
    defaults are not likely to be wrong for native compilation, don't mess with them
    
    and don't passthrough $LDFLAGS, etc., set the actual correct values from the wrappers with the right prefix

--- recipe/openmpi_activate.sh
+++ recipe/openmpi_activate.sh
@@ -1,17 +1,32 @@
 if [[ "${CONDA_BUILD:-}" = "1" ]]; then
   echo "setting openmpi environment variables for conda-build"
-  # build-time variables
-  for _var in CC CXX FC CPPFLAGS CFLAGS CXXFLAGS FCFLAGS LDFLAGS; do
-    if [[ ! -z "${!_var:-}" ]]; then
-      echo "OMPI_${_var}=${!_var}"
-      export OMPI_${_var}="${!_var}"
+  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" = "1" ]]; then
+    # set compilation variables during cross compilation
+    for _var in CC CXX FC; do
+      if [[ ! -z "${!_var:-}" ]]; then
+        echo "OMPI_${_var}=${!_var}"
+        export OMPI_${_var}="${!_var}"
+      fi
+    done
+    # require pkg-config?
+    if [[ -z "${OMPI_CFLAGS:-}" ]]; then
+      # pkg-config --cflags ompi
+      export OMPI_CFLAGS="-I$PREFIX/include"
     fi
-    if [[ -z "${OMPI_FCFLAGS}" && ! -z "${FFLAGS}" ]]; then
-      echo OMPI_FCFLAGS="-I$PREFIX/include $FFLAGS"
-      export OMPI_FCFLAGS="-I$PREFIX/include $FFLAGS"
+    if [[ -z "${OMPI_CXXFLAGS:-}" ]]; then
+      # pkg-config --cflags ompi-cxx
+      export OMPI_CXXFLAGS="-I$PREFIX/include"
     fi
-  done
-  export OPAL_PREFIX="$PREFIX"
+    if [[ -z "${OMPI_FCFLAGS:-}" ]]; then
+      # pkg-config --cflags ompi-fort
+      export OMPI_FCFLAGS="-I$PREFIX/include"
+    fi
+    if [[ -z "${OMPI_LDFLAGS:-}" ]]; then
+      # pkg-config --libs-only-L --libs-only-other ompi
+      export OMPI_LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+    fi
+    export OPAL_PREFIX="$PREFIX"
+  fi
 
   # runtime variables
   export OMPI_MCA_plm_ssh_agent=false
