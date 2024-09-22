533399ba6fe9ae2a612052767587f8694bc9ccd7
Jean Iaquinta <44261743+j34ni@users.noreply.github.com>
2024-09-22 22:56:12 +0200

    Update run_test.sh
    same test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -10,13 +10,13 @@ if [[ $PKG_NAME == "openmpi" ]]; then
 # -n : string is not null
 # -z : string is null, that is, has zero length
 
-# No UCX available on osx_64 and ppc64le platforms
-  if [[ "$target_platform" != "osx-64" && "$target_platform" != "linux-ppc64le" ]]; then
-      if [[ -z "$(ompi_info | grep ucx)" ]]; then
-          echo "OpenMPI configured without UCX support on $target_platform!"
-          exit 1
-      fi
-  fi
+    # No UCX available on osx_64 and ppc64le platforms
+    if [[ "$target_platform" != "osx-64" && "$target_platform" != "linux-ppc64le" ]]; then
+        if [[ -z "$(ompi_info | grep ucx)" ]]; then
+            echo "OpenMPI configured without UCX support on $target_platform!"
+            exit 1
+        fi
+    fi
 
   if [[ -z "$(ompi_info | grep cuda)" ]]; then
     echo "OpenMPI configured without CUDA support!"
